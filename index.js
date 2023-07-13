3b957fa3726dca9875d01b6aaccf72cce78a9386
Ruth Mackay <ruthiemackay@hotmail.com>
2023-07-13 11:53:36 +0100

    inner HTML city and description

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "7ecd2a71ced362676a580794d868a980";
